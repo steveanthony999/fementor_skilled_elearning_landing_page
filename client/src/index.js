aba676804b92a9f9ade54a5f4c1205b6e083918b
Steven Woodward <steveanthony999@gmail.com>
2022-09-11 20:44:35 -0700

    updated favicon, title, and set up scss

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import './dist/css/styles.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
